b6e6a0310cc7249ebf2f929d8a8d6512daa7a09c
Brett Gleason <brettmgleason@gmail.com>
2017-05-29 10:58:16 -0500

    fix: rover vision display
    Rover vision values were scaled 0-1 instead of 0-255

--- code/perception.py
+++ code/perception.py
@@ -114,9 +114,9 @@ def perception_step(Rover):
         # Example: Rover.vision_image[:,:,0] = obstacle color-thresholded binary image
         #          Rover.vision_image[:,:,1] = rock_sample color-thresholded binary image
         #          Rover.vision_image[:,:,2] = navigable terrain color-thresholded binary image
-    Rover.vision_image[:,:,0] = obs_threshed
-    Rover.vision_image[:,:,1] = sample_threshed
-    Rover.vision_image[:,:,2] = navigable_threshed
+    Rover.vision_image[:,:,0] = obs_threshed * 255
+    Rover.vision_image[:,:,1] = sample_threshed * 255
+    Rover.vision_image[:,:,2] = navigable_threshed * 255
 
     # 5) Convert map image pixel values to rover-centric coords
     xpix_rover_n, ypix_rover_n = rover_coords(navigable_threshed)
